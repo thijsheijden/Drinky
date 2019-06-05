5bde67ef18c387a6f96e491b5db10eb67869bd10
Thijs van der Heijden <hi@thijsheijden.nl>
2019-06-05 17:55:18 +0200

    Added notification for when the application enters the foreground and updating the UI then. This way, when the user adds a drink through the (today view?) or notification action the changes are instantly visible in the UI.

--- Drinky/Presenters/CurrentAmountDrankPresenter.swift
+++ Drinky/Presenters/CurrentAmountDrankPresenter.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class CurrentAmountDrankPresenter: PresenterProtocol {
     
@@ -25,6 +26,15 @@ class CurrentAmountDrankPresenter: PresenterProtocol {
         
         // check if there has been any activity today
         checkIfDayEntity()
+        
+        // setup an observer for when the application goes to the foreground so we know when to update the views
+        NotificationCenter.default.addObserver(self, selector: #selector(appCameToForeground), name: UIApplication.didBecomeActiveNotification, object: nil)
+    }
+    
+    // method which gets triggered when the applicatione enters the foreground, we need to update the ui then
+    @objc func appCameToForeground() {
+        checkIfDayEntity()
+        view.setupView()
     }
     
     // method to fill the drink view on the screen
